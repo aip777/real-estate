7528eea061dabd2f97ef9b473f4de949f62d8311
Ariful Islam Palash <ariful.islamk777@gmail.com>
2019-07-10 05:58:14 -0400

    add Design

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -5,7 +5,7 @@ from .models import blog
 
 
 class BlogAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title_one', 'description_one', 'is_published', 'photo_main', 'list_date')
+  list_display = ('id', 'title_one', 'description_one', 'is_published',   )
   list_display_links = ('id', 'title_one')
   list_filter = ('title_one',)
   list_editable = ('is_published',)
