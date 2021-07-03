2216f98bf3b11df294cd671b1a9fc02863deb1b9
aip <ariful.islamk777@gmail.com>
2021-07-03 13:51:56 +0600

    Contact text changed

--- btre/settings.py
+++ btre/settings.py
@@ -146,8 +146,8 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lai.wasson5487@gmail.com'
-EMAIL_HOST_PASSWORD ='456123lw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_SSL=False
 EMAIL_USE_TLS= True
 
